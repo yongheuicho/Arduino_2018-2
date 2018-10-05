cda134fffb701b77f9db45df0448ad5f1847149e
yongheuicho <yongheuicho@gmail.com>
2018-10-05 16:47:32 +0900

    [2018-10-05] study pinMode() and digitalWrite()

--- TestArduino/TestArduino.ino
+++ TestArduino/TestArduino.ino
@@ -2,14 +2,18 @@
 
 #define SERIAL_BPS  (9600)
 
+int nLed = 13;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(SERIAL_BPS);
+  pinMode(nLed, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Calculator calc;
-  String str = calc.scanOpFunStr();
-  prints(str); println();
+  digitalWrite(nLed, HIGH);
+  delay(1000);  // ms
+  digitalWrite(nLed, LOW);
+  delay(1000);  // ms
 }
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-10-05] start String keys"
+git commit -m "[2018-10-05] study pinMode() and digitalWrite()"
 git push -u origin master
