a7f4e6d77250b305650e08e3f381c4bea48c7f53
yongheuicho <yongheuicho@gmail.com>
2018-10-05 16:53:30 +0900

    [2018-10-05] study digitalRead()

--- TestArduino/TestArduino.ino
+++ TestArduino/TestArduino.ino
@@ -2,18 +2,18 @@
 
 #define SERIAL_BPS  (9600)
 
-int nLed = 13;
+int nInput = 7;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(SERIAL_BPS);
-  pinMode(nLed, OUTPUT);
+  pinMode(nInput, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(nLed, HIGH);
-  delay(1000);  // ms
-  digitalWrite(nLed, LOW);
-  delay(1000);  // ms
+  int nVolt = digitalRead(nInput);
+  if (nVolt == HIGH)  prints("High");
+  else  prints("Low");
+  delay(3000);  // ms
 }
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-10-05] study pinMode() and digitalWrite()"
+git commit -m "[2018-10-05] study digitalRead()"
 git push -u origin master
