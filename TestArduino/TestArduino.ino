f61f1beb28e376fc5a9eaf60623e70e0fb704dc3
yongheuicho <yongheuicho@gmail.com>
2018-10-25 17:20:51 +0900

    [2018-10-25] test getToken()

--- TestArduino/TestArduino.ino
+++ TestArduino/TestArduino.ino
@@ -2,19 +2,21 @@
 
 #define SERIAL_BPS  (9600)
 
-int nInput = 7;
-
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(SERIAL_BPS);
-  pinMode(nInput, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int nVolt = digitalRead(nInput);
-  if (nVolt == HIGH)  prints("High");
-  else  prints("Low");
-  println();
-  delay(3000);  // ms
+  StringTok stStr;
+  scans(stStr);
+  prints(stStr); println();
+  StringTok stToken;
+  stToken = stStr.getToken();
+  prints(stToken); println();
+  stToken = stStr.getToken();
+  prints(stToken); println();
+  stToken = stStr.getToken();
+  prints(stToken); println();
 }
--- mypush.bat
+++ mypush.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "[2018-10-25] start string command"
+git commit -m "[2018-10-25] test getToken()"
 git push -u origin master
